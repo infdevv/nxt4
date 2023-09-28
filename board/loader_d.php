26bf5fd531b3bd2203d0ddf0f79fd12b6a1f850a
infdevv <133452064+infdevv@users.noreply.github.com>
2023-09-28 06:56:03 -0500

    Update loader_d.php

--- board/loader_d.php
+++ board/loader_d.php
@@ -1,8 +1,17 @@
+
+<?php
 // Read the contents of data_m.json
-$json = file_get_contents('data_m.json');
+$json = file_get_contents('data_d.json');
 
 // Set the response content type to JSON
 header('Content-Type: application/json');
 
-// Output the JSON data
-echo $json;
+// Check if file_get_contents was successful
+if ($json === false) {
+    http_response_code(500); // Internal Server Error
+    echo json_encode(['error' => 'Unable to read data_d.json']);
+} else {
+    // Output the JSON data
+    echo $json;
+}
+?>
